2a4f9b122ca37c4ca3f22e599be232eea696612e
Mkrolick <67167906+Mkrolick@users.noreply.github.com>
2021-02-27 00:23:56 -0500

    Finished Template-Basics Exercises

--- Flask-Bootcamp-master/03-Template-Basics/07-Simple-Project.py
+++ Flask-Bootcamp-master/03-Template-Basics/07-Simple-Project.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 
 def represents_int(username):
-    try:
-        str(username)
-        return True
-    except Exception:
-        return False
+    for letter in username:
+        try:
+            str(letter)
+            return True
+        except Exception:
+            return False
 
 def string_is_upper_case(string):
     for letter in string:
@@ -25,10 +26,12 @@ def string_is_lower_case(string):
             pass
     return False
 
+
+
 @app.route('/')
 def index():
     # This home page should have the form.
-    return render_template('07-solution-base.index')
+    return render_template('07-solution-index.html')
 
 
 # This page will be the page after the form
@@ -36,15 +39,14 @@ def index():
 def report():
     username = request.args.get('username')
     # Check the user name for the 3 requirements.
-    if username[-1] == username[-1].upper() and string_is_upper_case(username) and string_is_lower_case(username):
-        return True
-    # HINTS:
-    # https://stackoverflow.com/questions/22997072/how-to-check-if-lowercase-letters-exist/22997094
-    # https://stackoverflow.com/questions/26515422/how-to-check-if-last-character-is-integer-in-raw-input
-
-    # Return the information to the report page html.
-    pass
-    render_template('07-solution-base.index', report=report)
+    if represents_int(username) and string_is_upper_case(username) and string_is_lower_case(username):
+        report = True
+    else:
+        report = False
+    upper = string_is_upper_case(username)
+    lower = string_is_lower_case(username)
+    num_end = represents_int(username)
+    return render_template('07-solution-report.html', upper=upper, lower=lower, num_end=num_end, report=report)
 
 if __name__ == '__main__':
     # Fill this in!
